90ff916162c1748902f4df29e8acb3cce841a3dc
liuq <it_liuq@163.com>
2023-08-21 21:21:48 +0000

    文件的读写

--- demo01.py
+++ demo01.py
@@ -9,7 +9,9 @@ LastEditTime: 2023-08-14 16:51:25
 import math
 from turtle import *
 import logging
-
+import random
+import requests
+import time
 
 degrees = 45
 x= math.sin(degrees / 360.0 * 2 * math.pi)
@@ -49,6 +51,7 @@ like = ['读书', '学python','学java']
 merit=('高','富','帅')
 dict={"degree":"大学"}
 print_twice("jerry",27,"男",like , merit, dict)
+print("like的长度：%s "%len(like))
 
 print("---------------------------------------")
 def square(t):
@@ -92,6 +95,151 @@ print('名称：%10s 年龄：%2d 身高：%0.2f' %(name ,age , sg))
 
 # f "{变量1}{变量2}"  快速格式化方法
 print(f'名称：{name}  年龄：{age}  身高：{sg}')
-
+#表达式的格式化
 print("jerry的类型是： %s" %type(name))
 #-------------------------------------------------------------------
+# age = input("请输入你的年龄：")
+
+# print("我是谁：%s" %age)
+# if int(age) > 30:
+#     print("你已经成年了！！")
+
+
+# feshu = input("请输入你的分数，然后评等级")
+
+# if int(feshu) <60:
+#     print("及格")
+# elif int(feshu) <90:
+#     print("中等")
+# elif int(feshu) < 100:
+#     print("优秀")
+# else:
+#     print("垃圾")
+
+# end=”“  不换行
+print("不",end='')
+print("换行")
+
+# \t 多个字符串变量对齐 相当与tab
+print("hello world")
+print("hi    jerry")
+
+print("hello\tworld")
+print("hi\tjerry")
+
+#-----------------------------------------------------------------------
+#range 语法 1  
+for x in range(10):
+    print(x)
+
+#range 语法 2 不含num2本身
+for x in range(5,10):
+    print(x)
+#range 语法 3 不含num2本身 step代表间隔
+for x in range(5,10,3):
+    print(x)
+
+num = random.randint(1,100)
+count = 0
+for x in range(1,num):
+    if x%2 == 0:
+        count+=1
+
+print(f"从1-{num}（不含num）共计出现偶数{count}次")
+
+#---------------------------------------------------------------
+# 函数的注释
+def add (x,y)->bool:
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        bool: _description_
+    """
+
+#-----------------------------------------------------------------
+#数据容器
+# 列表 list 可变数组 []
+list = [1,2,3]
+print(f"切片： {list[0:1]}") 
+# 元组 tuple 只读数组 () 单个数据（元读,)否则就是str
+t1 = ((1,2,3),(4,5,6))
+print(f"类型是： {type(t1)}   取值6：{t1[1][2]}")
+# 字符串 str  不可修改的数据容器
+s1 = " hello world "
+print(s1.replace('hello','hi')) #替换
+print(f"{s1.split(' ')}") # 切割
+print("%s"%{s1.strip()}) #去除前后空格
+print(f"h出现的次数：{s1.count('l')}")
+ 
+# 集合 set 不支持重复 {}  不支持下标索引访问
+set1={1,2,3,3}
+set2={1,2,3,34,5,6}
+print(f"{set}")
+print("%s" %set2.difference(set1))
+print(f"{set1.union(set2)}")
+
+#字典 {} json 
+
+d1 = {
+    name: 'jerry',
+    age: 27
+}
+#----------------------------------------------------------------
+# 函数的传惨 *age tuple 类型        **ages dict类型
+def test1():
+    print("方法1")
+
+def test2(x,y):
+    test1()
+
+#-------------------------------------------------------------------
+# 文件读取
+# open(name,mode,encoding) 
+# name 打开的文件名（包含路径）
+# mode 打开文件的模式（访问模式） r 只读   w 写入  a 追加
+# 编码格式 通常 TF-8
+file_name = '/home/kali/Downloads/del.sql'
+mode = 'r'
+encoding = 'UTF-8'
+f = open(file_name , mode=mode , encoding=encoding) 
+# print(type(f))
+# 读取文件 - read() 
+# print(f.read())
+
+# 读取文件readLines()  按行读取
+# fs = f.readlines(); # 和read 命令冲突
+# print(type(fs))
+# print("文件内容： %s " %fs )
+
+# 读取文件readLine()  按行读取
+# print(f"第1行{f.readline()}")
+# print(f"第2行{f.readline()}")
+
+# 关闭文件
+# time.sleep(3)
+f.close
+
+# whth open()  带自动close功能
+with open(file_name , mode='r' , encoding='UTF-8')  as file:
+    for f in file.readlines():
+        print(f"{f}")
+
+# write() 文件写入 
+# flush() 刷新缓存 从内存刷新到硬盘
+file_path = '/home/kali/Downloads/python.text' 
+file = open(file=file_path, mode="w", encoding="UTF-8") #w模式文件自动创建
+file.write("我爱学python！！！")
+file.flush()
+#time.sleep(10) # 休眠
+file.close # 文件关闭会自动持久化到硬盘
+
+# 追加写入
+
+file_path = '/home/kali/Downloads/python.text' 
+with open(file=file_path , mode="a" , encoding="UTF-8") as file:
+    file.write("\n我爱学java！！！")
+
--- task.py
+++ task.py
@@ -1,3 +1,5 @@
+import random
+import time
 
 name='ehl'
 stock_price=19.99
@@ -14,3 +16,124 @@ def countPrice(growth_day):
     print("每日增长系数：%0.1f，经过%d天的增长后，股价达到了：%0.2f"%(stock_price_daily_growth_fator,growth_day,price))
 
 countPrice(7)
+
+
+
+#定义一个数字1 ——10 ，随机产生，通过3次判断来猜出数字。
+# num = random.randint(1,10)
+# flag = True
+# while flag:
+#     u_num = int(input("你猜啊？   "))
+#     if u_num == num:
+#         print("猜对咯！ 这个数字是：  %s" %u_num)
+#         flag=False
+#     elif u_num > num:
+#         print("大了")
+#     else:
+#         print("小了")
+
+# 通过while 循环，输出99乘法表格
+i=1
+while i <= 9:
+    j= 1
+    while j<=i:
+        if j ==i:
+            print("%s*%s=%s"%(j,i,i*j))
+        else:
+            print(f"{j}*{i}={i*j}\t",end="")
+        j+=1
+    i +=1
+    print()
+        
+# for循环的冒泡排序 倒序
+array = []
+while len(array) < 10:
+    array.append(random.randint(1,100))
+print("%s"%array)
+def int_sort(array):
+    for i in range(1,len(array)):
+         for j in range(0,len(array)-i):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+print(int_sort(array))
+
+## 通过for 循环，输出99乘法表格
+for i in range(1,10):
+    for j in range(1,10):
+        #打印口诀
+        if j == i:
+            print(f"{j}*{i}={i*j}")
+            break
+        else:
+            print(f"{j}*{i}={i*j}",end="\t")
+    #输出玩1列换行
+    print()
+
+# python 冒泡排序 
+list = []
+for x in range(1,10):
+    list.append(random.randint(1,100))
+print("%s"%list)
+temp = 0
+def f_sort(list):
+    for i in range(0,len(list)):
+        for j in range(i+1,len(list)): 
+            if list[j]<list[i]:
+                temp = list[j]
+                list[j] = list[i]
+                list[i] = temp
+    return list;
+print("%s" %f_sort(list))
+
+# 统计某个文件中单词出现的数量
+def countDict(file_name ,key_words):
+    file = open(file=file_name , mode= 'r' , encoding= 'UTF-8')
+    file_content = file.read(); 
+    return file_content.count(key_words)
+    
+file_name = '/home/kali/Downloads/del.sql'
+key_words= 'kubectl'
+print(countDict(file_name, key_words))
+
+# 备份账单并删除测试数据
+
+def write(file_name , content):
+    """_summary_
+
+    Args:
+        file_name (_type_): _description_ 文件路径
+        content (_type_): _description_ 文件内容
+    """
+    with open(file_name ,mode="w",encoding="UTF-8") as file:
+        file.writelines(content)
+content = []
+
+for i in range(1 , 10):
+    content.append(str(i) +"\t")
+    content.append("jerry\t")
+    # time.sleep(1)
+    content.append(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())+"\t")
+    if i%2 == 0:
+        content.append("测试\n")
+    else:
+        content.append("正式\n")
+
+
+file_name2 = '/home/kali/Downloads/bill.text'
+write(file_name2,content)
+
+def delete_content(file_name2, key_words):
+    newContent = []
+    with open(file_name2, mode="r",encoding="UTF-8") as file:
+        content = file.readlines()
+        for line in content:
+            if line.count(key_words) != 1:
+                newContent.append(line)
+    print(f"{newContent}")
+
+    with open(file_name2, mode="w",encoding="UTF-8") as newF:
+        newF.writelines(newContent)
+
+key_words = "测试"
+delete_content(file_name2,key_words)
