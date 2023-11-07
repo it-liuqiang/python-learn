0715fa1f587797d75bdbc64c0077ab70bd18b6bb
liuq <liuq03@ehualu.com>
2023-11-07 20:57:26 +0800

    fix dir

--- contant/contants.py
+++ contant/contants.py
@@ -1,1 +1,9 @@
+'''
+Author: liuq liuq03@ehualu.com
+Date: 2023-08-23 11:12:38
+LastEditors: liuq liuq03@ehualu.com
+LastEditTime: 2023-11-07 20:41:54
+FilePath: \python-learn\contant\contants.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 YYYY_MM_DD_HHMMSS = "%Y-%m-%d %H:%M:%S"
