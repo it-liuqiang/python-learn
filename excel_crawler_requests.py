55b5687976f4eb001bd4982950c93888c612f97f
liuq <liuq03@ehualu.com>
2023-11-07 20:46:18 +0800

    暂时

--- excel_crawler_requests.py
+++ excel_crawler_requests.py
@@ -1,11 +1,3 @@
-'''
-Author: liuq liuq03@ehualu.com
-Date: 2023-08-24 16:01:12
-LastEditors: liuq liuq03@ehualu.com
-LastEditTime: 2023-08-24 16:01:30
-FilePath: \python-learn\excel_crawler_requests.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 # file_name: excel_crawler_requests.py
 import requests
 import pandas as pd
--- read_excel.py
+++ read_excel.py
@@ -2,6 +2,14 @@
 Author: liuq liuq03@ehualu.com
 Date: 2023-08-24 09:52:47
 LastEditors: liuq liuq03@ehualu.com
+LastEditTime: 2023-08-25 09:33:48
+FilePath: \python-learn\read_excel.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+'''
+Author: liuq liuq03@ehualu.com
+Date: 2023-08-24 09:52:47
+LastEditors: liuq liuq03@ehualu.com
 LastEditTime: 2023-08-24 15:39:08
 FilePath: \python-learn\read_excel.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -16,35 +24,45 @@ import numpy as numpy
 
 
 
-file_name="D:\\jerry\\事件码-字典&新增记录.xlsx"
+file_name="D:\\jerry\\算法能力对比.xlsx"
+
+#========================================使用pandas解析==============================================================
+
+# content = pandas.read_excel(file_name,header=2,sheet_name="Sheet1")
+# # print(content)
+# print(list(content))
+# # print(numpy.array(content)) 
+# # print(content.get("序号"))
+# list = []
+# for row_list in numpy.array(content):
+#     print(row_list)
+#     dict = {
+#             'algorithmName':row_list[1],
+#             'algorithmId':row_list[2]
+#             }
+#     list.append(dict)
+
+# # print(list)  
+# print(type(json.dumps(list)))
+# print(json.dumps(list,ensure_ascii=False))
 
 
-
-content = pandas.read_excel(file_name,header=0,sheet_name="0-全目标")
-# print(content)
-print(list(content))
-# print(numpy.array(content)) 
-# print(content.get("序号"))
-list = []
-for row_list in numpy.array(content):
-    print(row_list)
-    dict = {'序号':row_list[0],
-            '编码':row_list[1],
-            '名称':row_list[2],
-            '类型':row_list[3],
-            '备注':row_list[4]
-            }
-    list.append(dict)
-
-# print(list)  
-print(type(json.dumps(list)))
-print(json.dumps(list,ensure_ascii=False))
-
-
-# wk = openpyxl.load_workbook(file_name)
-# sheet     = wk.get_sheet_by_name("0-全目标")
+wk = openpyxl.load_workbook(file_name)
+sheet     = wk.get_sheet_by_name("Sheet1")
 # print(sheet.max_row)
-# for r in range(0,sheet.max_row):
-#     row=[item.value for item in list(sheet.rows)[r]]
-#     print(f'第{r}行值',row)
+array = []
+for r in range(0,sheet.max_row):
+    row=[item.value for item in list(sheet.rows)[r]]
+    # print(f'第{r}行值',row)
+    dict = {
+            'algorithmId':row[2],
+            'resultNum':row[13],
+            'accuracy':row[14]
+            }
+    array.append(dict)
 
+d1 = {
+    'provider':'002373',
+    'algoList': array
+}
+print(json.dumps(d1))
