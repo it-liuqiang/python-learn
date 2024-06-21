6666b6f331f3a8b0de958d4053375fce34b5df63
liuq <liuq_wb@ehualu.com>
2024-06-21 10:03:20 +0800

    1

--- 大数据/count_plan_data.py
+++ 大数据/count_plan_data.py
@@ -1,18 +1,8 @@
-'''
-Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-Date: 2024-06-17 10:01:30
-LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2024-06-18 08:58:38
-FilePath: \python-learn\大数据\count_plan_data.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
-# 根据条件统计信号方案
+
 import json
-import logging
 import util.file_util as file_util
 import pandas as pandas
 import openpyxl
-import xlrd
 import numpy as numpy
 
 if __name__ == '__main__':
